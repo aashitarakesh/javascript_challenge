28bdc76be3ebd1db70f5d7b8da46d261c36f1aa9
aashitarakesh <aashita@hike.health>
2021-04-10 20:31:05 -0400

    Adding reset search

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,33 @@
 // from data.js
 var tableData = data;
 
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
 // Select the button
 var button = d3.select("#filter-btn");
+var reset = d3.select("#reset-btn")
 
 // Select the form
-var form = d3.select("#form");
+var form = d3.select("#datetime");
+
+// Populate the table using UFO dataset
+function createTable() {
+  tableData.forEach(date =>{
+  console.log(date)
+  var tr = tbody.append("tr");
+  Object.entries(date).forEach(([key,value])=> {
+      console.log(`key = ${key} value=${value}`)
+      tr.append("td").text(value)
+  });
+     
+});
+d3.select("table").attr("class"," table table-striped")
+};
 
 // Create event handlers 
 button.on("click", runEnter);
+reset.on("click", resetSearch);
 form.on("submit",runEnter);
 
 // Create the function to run for both events
@@ -16,6 +35,7 @@ function runEnter() {
 
 // Prevent the page from refreshing
     d3.event.preventDefault();
+
 
 // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
@@ -32,23 +52,28 @@ function runEnter() {
 
 //   console.log(filteredData);
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
  // clear table before appending filtered data
  tbody.html("");
 
 // Appending filtered data to table 
 
-filteredData.forEach(date =>{
-    console.log(date)
-    var tr = tbody.append("tr")
-    Object.entries(date).forEach(([key,value])=> {
+filteredData.forEach(ufoSighting =>{
+    console.log(ufoSighting)
+    var tr = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(([key,value])=> {
         console.log(`key = ${key} value=${value}`)
         tr.append("td").text(value)
-    })
+    });
        
-})
-d3.select("table").attr("class"," table table-striped")
+});
 
 };
+
+// Create the function to reset search values and table
+function resetSearch() {
+  document.getElementById("myForm").reset();  
+  // render table
+  createTable();
+};
+// Render UFO table for the first time on page load
+createTable();
